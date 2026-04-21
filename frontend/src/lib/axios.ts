42698da1474c349e70851904f13c085cf41363f5
Mridul Thakur <mridul.t99@gmail.com>
2026-04-21 18:54:35 +0530

    changes

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,5 @@
-import axios from 'axios';
-export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true,
-});
+import axios from "axios";
 
-export default axiosInstance;
+export const axiosInstance = axios.create({
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+});
